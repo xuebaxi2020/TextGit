d6e17c04fb86dda882705d9d624ee8b476463774
xuebaxi2020 <1174742080@qq.com>
2020-05-15 22:59:53 +0800

    this is new project

--- src/main/java/com/atyx/git/apple.java
+++ src/main/java/com/atyx/git/apple.java
@@ -5,5 +5,6 @@ public class apple {
 			public String name;
 			public static void main(String[] args) {
 				System.out.println("this is hot_fix...");
+				System.out.println("this new ");
 			}
 }
